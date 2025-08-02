b9b5dc6b9ca1fa2e16661f14a7c814008ab394be
jmohren <Johannes.mohren@rwth-aachen.de>
2025-08-02 20:52:02 +0200

    added machine data

--- src/app/utils/recurringTasks.ts
+++ src/app/utils/recurringTasks.ts
@@ -1,4 +1,4 @@
-import { addDays, addWeeks, addMonths, addYears, isAfter, isBefore, isSameDay, parseISO, format } from 'date-fns';
+import { addDays, addWeeks, addMonths, addYears, isAfter, isBefore, isSameDay, format } from 'date-fns';
 import { Task, Machine } from '@/app/state/MachineProvider';
 
 export interface TaskOccurrence {
