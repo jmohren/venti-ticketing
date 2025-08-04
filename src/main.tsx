77ae97a4c3c16dff56c44a3ae733891dd0b45ad0
jmohren <Johannes.mohren@rwth-aachen.de>
2025-08-04 07:58:43 +0200

    old styling

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,7 @@ import { UserProvider } from '@/core/state/UserProvider.tsx'
 const theme = createTheme({
   palette: {
     primary: {
-      // main: '#286982', // Your custom primary color 
-      main: '#2B67AD', // Your custom primary color 
+      main: '#286982', // Your custom primary color 
     },
   },
 });
